894e9edea040741be8fcef450d2063ed8b935685
Naimur53 <naimurrhman53@gmail.com>
2021-03-07 14:59:38 +0600

    all problem solved

--- elemants/js/main.js
+++ elemants/js/main.js
@@ -128,118 +128,13 @@
             columnWidth: '.grid-sizer'
         }
     })
-    //filtering button
-    $('.filter-button-group').on('click', 'button', function() {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
-    // blog-active
-    $('.blog-content').isotope({
-        itemSelector: '.blog-single',
-        percentPosition: true,
-        masonry: {
-            // use outer width of grid-sizer for columnWidth
-            columnWidth: '.blog-sizer'
-        }
-    })
-    //parallaxie 
-    $('.parallaxie').parallaxie();
+    
 
-    //map funciton
-    function basicmap() {
-        // Basic options for a simple Google Map
-        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-        var mapOptions = {
-            // How zoomed in you want the map to start at (always required)
-            zoom: 11,
-            scrollwheel: false,
-            // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(40.6700, -73.9400), // New York
-            // This is where you would paste any style found on Snazzy Maps.
-            styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": .2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }]
-        };
-        // Get the HTML DOM element that will contain your map 
-        // We are using a div with id="map" seen below in the <body>
-        var mapElement = document.getElementById('map');
 
-        // Create the Google Map using our element and options defined above
-        var map = new google.maps.Map(mapElement, mapOptions);
 
-        // Let's also add a marker while we're at it
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(40.6700, -73.9400),
-            map: map,
-            title: 'Cryptox'
-        });
-    }
-    if ($('#map').length != 0) {
-        google.maps.event.addDomListener(window, 'load', basicmap);
-    }
 
-    //js for comments section
-    $(".reply-button1").click(function() {
-        $(".post-media-1").slideToggle(700);
-    })
-    $(".reply-button2").click(function() {
-        $(".post-media-2").slideToggle(700);
-    })
-    $(".reply-button3").click(function() {
-        $(".post-media-3").slideToggle(700);
-    })
 
-    //load more js
-    $(".media-hidden").slice(0, 3).show();
-    $("#loadMore").on('click', function(e) {
-        e.preventDefault();
-        $(".media-hidden:hidden").slice(0, 1).slideDown();
-        if ($(".media-hidden:hidden").length == 0) {
-            $("#loadMore").fadeOut('slow');
-        }
-    });
 
-    //one page scrolling js
-    function scrollToSection(event) {
-        event.preventDefault();
-        var $section = $($(this).attr('href'));
-        $('html, body').animate({
-            scrollTop: $section.offset().top
-        }, 500);
-    }
-    $('[data-scroll]').on('click', scrollToSection);
 
-    //bg change of navbar
-    $(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-      if (scroll > 840) {
-        $(".navbar").css({"background-color": "yellow", "transition": ".7s"});
-      }
-
-      else{
-          $(".navbar").css("background" , "transparent");   
-      }
-      if (scroll > 480) {
-        $(".blog-single-nav").css({"background-color": "yellow", "transition": ".7s"});
-      }
-
-      else{
-          $(".blog-single-nav").css("background" , "transparent");   
-      }
-  })
-
-    // scroll to up
-     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scrollup').fadeIn();
-        } else {
-            $('.scrollup').fadeOut();
-        }
-    });
-
-    $('.scrollup').click(function () {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 600);
-        return false;
-    });
-
+    
 })(jQuery);
